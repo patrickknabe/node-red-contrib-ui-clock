16ecb5c1b678458daa129e16a133341dd1df3ea3
Patrick Knabe <patrickknabe@gmail.com>
2020-10-01 06:27:55 +0200

    Optimizations

--- clock.js
+++ clock.js
@@ -1,10 +1,14 @@
 module.exports = RED => {
-	try {
-		var ui = RED.require( 'node-red-dashboard' )( RED );
-	} catch {}
+	let ui;
 
 	RED.nodes.registerType( 'ui_clock', function( config ) {
 		RED.nodes.createNode( this, config );
+
+		if( !ui ) {
+			try {
+				ui = RED.require( 'node-red-dashboard' )( RED );
+			} catch {}
+		}
 
 		if( ui && RED.nodes.getNode( config.group ) ) {
 			this.on( 'close', ui.addWidget( {
@@ -26,28 +30,25 @@ module.exports = RED => {
 							<circle cx="262.5" cy="88.638" r="25" />
 						</g>
 						<g style="stroke: var( --nr-dashboard-widgetTextColor );">
-							<line id="ui_clock_{{ $id }}_hours" x1="500" y1="500" x2="500" y2="200" stroke-linecap="round" stroke-width="30" />
-							<line id="ui_clock_{{ $id }}_minutes" x1="500" y1="500" x2="500" y2="75" stroke-linecap="round" stroke-width="20" />
+							<line id="ui_clock_{{ $id }}_h" x1="500" y1="500" x2="500" y2="200" stroke-linecap="round" stroke-width="30" />
+							<line id="ui_clock_{{ $id }}_m" x1="500" y1="500" x2="500" y2="75" stroke-linecap="round" stroke-width="20" />
 						</g>
 						<g style="stroke: var( --nr-dashboard-groupTextColor );">
-							<line id="ui_clock_{{ $id }}_seconds" x1="500" y1="500" x2="500" y2="25" stroke-linecap="round" stroke-width="10" />
+							<line id="ui_clock_{{ $id }}_s" x1="500" y1="500" x2="500" y2="25" stroke-linecap="round" stroke-width="10" />
 						</g>
 					</svg>
+					<input type="hidden" value="{{ msg.payload }}" />
 				`,
 				width: config.width,
 				height: +config.height || RED.nodes.getNode( config.group ).config.width,
 				group: config.group,
 				order: config.order,
-				templateScope: 'local',
-				beforeEmit: msg => ( { msg } ),
-				initController: $scope => $scope.$watch( 'msg', msg => {
-					if( msg && typeof msg.payload === 'number' ) {
-						const date = new Date( msg.payload );
+				initController: $scope => $scope.$watch( 'msg.payload', payload => {
+					const date = new Date( +payload || 0 );
 
-						$( `#ui_clock_${ $scope.$id }_seconds` ).attr( 'transform', `rotate( ${ date.getSeconds() * 6 }, 500, 500 )` );
-						$( `#ui_clock_${ $scope.$id }_minutes` ).attr( 'transform', `rotate( ${ ( date.getMinutes() + date.getSeconds() / 60 ) * 6 }, 500, 500 )` );
-						$( `#ui_clock_${ $scope.$id }_hours` ).attr( 'transform', `rotate( ${ ( date.getHours() + date.getMinutes() / 60 ) * 30 }, 500, 500 )` );
-					}
+					$( `#ui_clock_${ $scope.$id }_s` ).attr( 'transform', `rotate( ${ date.getSeconds() * 6 }, 500, 500 )` );
+					$( `#ui_clock_${ $scope.$id }_m` ).attr( 'transform', `rotate( ${ ( date.getMinutes() + date.getSeconds() / 60 ) * 6 }, 500, 500 )` );
+					$( `#ui_clock_${ $scope.$id }_h` ).attr( 'transform', `rotate( ${ ( date.getHours() + date.getMinutes() / 60 ) * 30 }, 500, 500 )` );
 				} )
 			} ) );
 		}
